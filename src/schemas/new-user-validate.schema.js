3d93aabf94a4b892f3294fcc99c3d020652dfc6d
dmysorenagaraj <dmysorenagaraj@hubgroup.com>
2019-11-22 21:24:47 -0600

    access token not required

--- src/schemas/new-user-validate.schema.js
+++ src/schemas/new-user-validate.schema.js
@@ -5,7 +5,6 @@ module.exports = async function (obj) {
         username: Joi.string().alphanum().min(3).max(30).required(),
         password: Joi.string().pattern(/^[a-zA-Z0-9]{3,30}$/),
         repeat_password: Joi.ref('password'),
-        access_token: [Joi.string(), Joi.number()],
         birth_year: Joi.number().integer().min(1900).max(2013),
         email: Joi.string().email()
     }).with('username', 'birth_year');
