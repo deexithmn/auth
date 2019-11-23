2f39bd4079346003afe0129a1e226ce6e58a953d
dmysorenagaraj <dmysorenagaraj@hubgroup.com>
2019-11-22 21:13:38 -0600

    beautify

--- src/service/verify-user.service.js
+++ src/service/verify-user.service.js
@@ -4,7 +4,6 @@ module.exports = function (req, res, next) {
     try {
         const token = req.header('Authorization');
         if (!token) throw new Error('Access denied');
-
         const userId = jwt.verify(token, process.env.AUTH_SECRET);
         req.userId = userId;
         next();
